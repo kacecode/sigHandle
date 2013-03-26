675abbeef3715a938a31ac34b225b5705f6c1279
DivinusVox <kacey@purplestate>
2013-03-25 21:47:08 -0600

    Manually merged main and mapugina fork

--- project5.c
+++ project5.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -93,51 +92,6 @@ void parentSigHandle( int sigid )
   default:
     printf( "PARENT: Weird stuff in the pipe - received signal id %d\n", sigid );
   }
-=======
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/wait.h>
-
-
-void parentAction( int childpid );
-void childAction( );
-void parentSigHandle( int sigid );
-void childSigHandle( int sigid );
-
-void main()
-{
-	//fork
-	//figure out if parent or child
-		//0 => childAction()
-		//-1 => error
-		//else => parentAction()
-}
-
-// parentAction tree
-// takes pid of child for reference
-// no conditions
-void parentAction( int childpid )
-{
-}
-
-// parentSigHandle
-// bound to parentAction signal handling events
-void parentSigHandle( int sigid )
-{
-  switch ( sigid )
-  {
-  case SIGUSR1:
-    printf( "*** Parent SIGUSR1 handler - Received 'task started' signal from child ***\n\n" );
-    break;
-  case SIGUSR2:
-    printf( "*** Parent SIGUSR2 handler - Received 'task completed' signal from child ***\n\n" );
-    break;
-  default:
-    printf( "PARENT: Weird stuff in the pipe - received signal id %d\n", sigid );
-  }
 }
 
 // ChildAction
@@ -194,5 +148,3 @@ void childSigHandle( int sigid )
 	default:
 		printf( "CHILD: Unexpected child signal id %d\n", sigid);
 	}
->>>>>>> 8daf1db895d4d0dcec3ca0bac4e7f8752e5f5ff7
-}
